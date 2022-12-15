a6ddb74b8b8c66206c82079852679419d56b8a4d
Frank Pizzella <fpizz210@gmail.com>
2022-12-15 18:35:04 -0500

    Resume Link

--- portfolio_react/src/components/Navbar/Navbar.tsx
+++ portfolio_react/src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { HiMenuAlt4, HiX} from 'react-icons/hi';
 import { motion } from 'framer-motion';
+import { NavLink, useLocation } from 'react-router-dom';
 
 import { images } from "../../constants";
 import './Navbar.scss';
@@ -9,19 +10,31 @@ const Navbar = () => {
     const [toggle, setToggle] = useState(false);
 
     return (
-        <nav className="app__navbar">
+        <nav className="app__navbar" style={{padding: useLocation().pathname === '/resume' ? '1.5rem 2rem' : ''}}>
             <div className="app__navbar-logo">
-                <img src={images.frankTitle} alt="logo" />
+                <NavLink to="/">
+                    <img src={images.frankTitle} alt="logo" />
+                </NavLink>
             </div>
-            <div className="app__navbar-links">
-                <ul>
-                    {['home', 'about', 'skills', 'resume', 'contact'].map((item, index) => (
-                        <li className="app__flex p-text" key={`link-${item}`}>
-                            <div />
-                            <a href={`#${item}`}>{item}</a>
-                        </li>
-                    ))}
-                </ul>
+            {
+                useLocation().pathname !== '/resume' && (
+                    <div className="app__navbar-links">
+                        <ul>
+                            {['home', 'about', 'skills', 'contact'].map((item, index) => (
+                                <li className="app__flex p-text" key={`link-${item}`}>
+                                    <div />
+                                    <a href={`#${item}`}>{item}</a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )
+            }
+            
+            <div className="app__navbar-resume">
+                <NavLink className="app__navbar-resume-link" to="/resume">
+                    <h3>Resume</h3>  
+                </NavLink>           
             </div>
             <div className="app__navbar-menu">
                 <HiMenuAlt4 onClick={() => setToggle(true)}/>
@@ -32,7 +45,7 @@ const Navbar = () => {
                     >
                         <HiX onClick={() => setToggle(false)}/>
                         <ul>
-                            {['home', 'about', 'skills', 'resume', 'contact'].map((item) => (
+                            {['home', 'about', 'skills', 'contact'].map((item) => (
                                 <li key={item}>
                                     <a href={`#${item}`} onClick={() => setToggle(false)}>
                                         {item}
---
+++ portfolio_react/src/container/Resume/Resume.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+import { AppWrap } from "../../wrapper";
+import { images } from "../../constants";
+import './Resume.scss';
+
+const Resume = () => {
+    return(
+        <div className="app__resume app__flex">
+            <h1>Resume</h1>
+        </div>
+    );
+}
+
+export default Resume;
