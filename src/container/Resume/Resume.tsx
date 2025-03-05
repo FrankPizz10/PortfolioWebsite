e8aa83e5aa4cdfe4f429718cae9e1b65054483d6
Frank Pizzella <fpizz210@gmail.com>
2025-03-05 16:56:13 -0500

    Resume Update

--- src/container/Resume/Resume.tsx
+++ src/container/Resume/Resume.tsx
@@ -18,31 +18,28 @@ const Resume = () => {
                 <div className="nuvera">
                     <h3>City of Cambridge <span>Web Developer	/ May 2023 – Present</span></h3>
                     <ul>
-                        <li><p>Utilized tech stack consisting of .NET MVC, Sitecore, Bitbucket, JavaScript</p></li>
+                        <li><p>Utilize tech stack consisting of .NET MVC, Sitecore, Bitbucket, JavaScript, Azure</p></li>
                         <li><p>Improve, maintain, and modernize the main City of Cambridge website</p></li>
-                        <li><p>Integrate Sitecore controls, renderings, and templates to allow staff to easily add content</p></li>
-                        <li><p>Push new biweekly releases to the website to keep up with requests from various departments</p></li>
-                        <li><p>Establish new testing suites including Visual Regression Testing</p></li>
+                        <li><p>Integrate Sitecore controls, renderings, and templates to allow staff to add content</p></li>
+                        <li><p>Develop full-stack applications such as financial disclosure software for electoral candidates</p></li>
+                        <li><p>Modernize our infrastructure by moving applications to Azure with a hybrid-cloud approach</p></li>
                     </ul>
                 </div>
                 <div className="nuvera">
                     <h3>Nuvera Fuel Cells <span>Software Engineering CO-OP	/ July 2022 – December 2022</span></h3>
                     <ul>
                         <li><p>Utilized tech stack consisting of Python, Scikit-Learn, Pandas</p></li>
-                        <li><p>Processed, filtered, and plotted 3D data for fuel cell engines</p></li>
-                        <li><p>Discovered correlations between operating conditions and engine performance</p></li>
-                        <li><p>Implemented machine learning regression algorithms to predict future engine performance</p></li>
+                        <li><p>Processed, filtered, and plotted 3D data for fuel cell engine performance</p></li>
+                        <li><p>Applied ML regression algorithms to find correlations between conditions and engine performance</p></li>
                     </ul>
                 </div>
                 <div className="philips">
                     <h3>Philips North America <span>Software Engineering CO-OP / July 2021 – December 2021</span></h3>
                     <ul>
                         <li><p>Utilized tech stack consisting of C#, SQL, Azure DevOps</p></li>
-                        <li><p>Enhanced an CI pipeline to test performance of Philips Patient Information Center (PIC IX)</p></li>
+                        <li><p>Enhanced a CI pipeline to test the performance of Philips Patient Information Center (PIC IX)</p></li>
                         <li><p>Created Build and Release pipelines to deploy the PIC IX on a network system</p></li>
-                        <li><p>Ran a series of tests on the product, collected metrics, and published results</p></li>
-                        <li><p>Updated outdated SQL queries and C# scripts to increase traceability of errors and efficiency</p></li>
-                        <li><p>Leveraged the Azure DevOps REST API to create a work item automatically </p></li>
+                        <li><p>Modernized deprecated SQL queries and C# scripts to increase traceability of errors and efficiency</p></li>
                     </ul>
                 </div>
                 <div className="insulet">
@@ -51,8 +48,7 @@ const Resume = () => {
                         <li><p>Utilized tech stack consisting of Python, Pandas, and MATLAB</p></li>
                         <li><p>Generated Python scripts using Pandas and MATLAB scripts to analyze clinical data</p></li>
                         <li><p>Tested the Horizon Omnipod 5 System extensively in a formal Design Verification process</p></li>
-                        <li><p>Designed a Continuous Glucose Monitor simulation </p></li>
-                        <li><p>Tested key protocols such as algorithm function, insulin delivery, and pod communications</p></li>
+                        <li><p>Designed a Continuous Glucose Monitor simulation using Python</p></li>
                     </ul>
                 </div>
                 <div className="insulet">
@@ -70,28 +66,20 @@ const Resume = () => {
                     <h3>BeerPassport <span>Solo App Developer / June 2023 - Present</span></h3>
                     <ul>
                         <li><p>Utilized tech stack consisting of React Native, Node.js, Express, MySQL, Prisma, AWS, Docker</p></li>
-                        <li><p>Developed an engaging beer tracking app on the App Store, powered by a custom AWS server</p></li>
-                        <li><p>Created features enabling users to track tried beers, complete collections, and connect with friends</p></li>
-                        <li><p>Configured a VPC to include an ECS cluster, EFS storage, and an Application Load Balancer</p></li>
-                        <li><p>Implemented an admin frontend to continuously update the existing 5000 strong beer database</p></li>
-                        <li><p>Leveraged Firebase for frontend sign-in with server middleware to validate authentication tokens</p></li>
-                    </ul>
-                </div>
-                <div className="muscle-map">
-                    <h3>Senior Capstone <span>HuskyCast Team Lead / January 2023 - May 2023</span></h3>
-                    <ul>
-                        <li><p>Utilized tech stack consisting of React.js (Typescript), Node.js, Express, PostgreSQL, Prisma, AWS</p></li>
-                        <li><p>Contributed as team leader to complete a baseball tracking system for little leaguers</p></li>
-                        <li><p>Created an Express Server, PostgreSQL database, AWS S3 bucket, and CloudFront distribution</p></li>
-                        <li><p>Connected a React fronted for users to view baseball cards and stats</p></li>
+                        <li><p>Developed an open-source beer tracking app on the App Store, powered by a custom AWS server</p></li>
+                        <li><p>Created features for beer tracking, collection completion, and social connectivity</p></li>
+                        <li><p>Configured AWS VPC with ECS cluster, EFS storage, and Application Load Balancer</p></li>
+                        <li><p>Implemented an admin frontend for ongoing updates to a 6,000+ beer database</p></li>
+                        <li><p>Leveraged Firebase for frontend authentication with JWT validation in server-side middleware</p></li>
                     </ul>
                 </div>
             </div>
             <div className='app__resume-certifications'>
-                <h1>Certifications</h1>
+                <h1>Additional</h1>
                 <div className="certifications">
                     <ul>
-                        <li><p>Sitecore 10 Developer Certification</p></li>
+                        <li><p><strong>Volunteering: </strong>Assisted the Maverick Foundation as a Web Consultant to help them launch a new website</p></li>
+                        <li><p><strong>Certifications: </strong>Sitecore 10, Microsoft Certified: Azure Fundamentals</p></li>
                     </ul>
                 </div>
             </div>
